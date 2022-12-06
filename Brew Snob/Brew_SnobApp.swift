9486c0b4ebfb04e299e78671905460d1795ce8cb
India Doria <60370426+clamquarter@users.noreply.github.com>
2022-12-06 16:35:25 -0500

    Initial Commit

---
+++ Brew Snob/Brew_SnobApp.swift
@@ -0,0 +1,17 @@
+//
+//  Brew_SnobApp.swift
+//  Brew Snob
+//
+//  Created by India Doria on 12/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Brew_SnobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
